582d5dd4946c798f0770d9388339731149df2356
Shohei <yukawa.shouhei@gmail.com>
2025-04-05 20:26:01 +0900

    横方向の伸縮のアニメーション追加

--- swiftfulBootcamp/StepperBootcamp.swift
+++ swiftfulBootcamp/StepperBootcamp.swift
@@ -10,9 +10,29 @@ import SwiftUI
 struct StepperBootcamp: View {
     
     @State var stepperValue: Int = 10
+    @State var widthIncrement: CGFloat = 0
     
     var body: some View {
-        Stepper("Stepper", value: $stepperValue)
+        VStack {
+            Stepper("Stepper: \(stepperValue)", value: $stepperValue)
+                .padding(50)
+            
+            RoundedRectangle(cornerRadius: 25.0)
+                .frame(width: 100 + widthIncrement,height: 100)
+            
+            Stepper("Stepper 2") {
+                widthIncrement += 10
+                incrementWidth(amount: 10)
+            } onDecrement: {
+                incrementWidth(amount: -10)
+            }
+            .padding(50)
+        }
+    }
+    func incrementWidth (amount: CGFloat) {
+        withAnimation(.easeInOut) {
+            widthIncrement += amount
+        }
     }
 }
 
