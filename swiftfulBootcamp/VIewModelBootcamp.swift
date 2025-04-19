16b2b9b0b7d15794ab92bd0f86fc7ed79ae09573
Shohei <yukawa.shouhei@gmail.com>
2025-04-19 19:52:38 +0900

    ViewModel リロード画面実装

--- swiftfulBootcamp/VIewModelBootcamp.swift
+++ swiftfulBootcamp/VIewModelBootcamp.swift
@@ -13,47 +13,65 @@ struct FruitModel: Identifiable {
     let count: Int
 }
 
-struct ViewModelBootcamp: View {
+class FruitViewModel:ObservableObject {
     
-    @State var fruitArray: [FruitModel] = [
-        FruitModel(name: "Apples", count: 5)
-    ]
+    @Published var fruitArray: [FruitModel] = []
+    @Published var isLoading: Bool = false
     
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(fruitArray) { fruit in
-                    HStack {
-                        Text("\(fruit.count)")
-                            .foregroundStyle(.red)
-                        Text(fruit.name)
-                            .font(.headline)
-                            .bold()
-                    }
-                }
-                .onDelete(perform: deleteFruit)
-            }
-            .listStyle(GroupedListStyle())
-            .navigationTitle("Fruit List")
-            .onAppear {
-                getFruits()
-            }
-        }
-    }
     func getFruits() {
         let fruit1 = FruitModel(name: "Orange", count: 1)
         let fruit2 = FruitModel(name: "Banana", count: 2)
         let fruit3 = FruitModel(name: "Watermelon", count: 28)
         let fruit4 = FruitModel(name: "Strawberry", count: 8)
         
-        fruitArray.append(fruit1)
-        fruitArray.append(fruit2)
-        fruitArray.append(fruit3)
-        fruitArray.append(fruit4)
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            self.fruitArray.append(fruit1)
+            self.fruitArray.append(fruit2)
+            self.fruitArray.append(fruit3)
+            self.fruitArray.append(fruit4)
+            self.isLoading = false
+        }
+        
     }
+    
     func deleteFruit(index: IndexSet) {
         fruitArray.remove(atOffsets: index)
     }
+}
+
+struct ViewModelBootcamp: View {
+    
+    @ObservedObject var fruitViewModel: FruitViewModel = FruitViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                
+                if fruitViewModel.isLoading {
+                    ProgressView()
+                } else {
+                    ForEach(fruitViewModel.fruitArray) { fruit in
+                        HStack {
+                            Text("\(fruit.count)")
+                                .foregroundStyle(.red)
+                            Text(fruit.name)
+                                .font(.headline)
+                                .bold()
+                        }
+                    }
+                    
+                    .onDelete(perform: fruitViewModel.deleteFruit)
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Fruit List")
+            .onAppear {
+                fruitViewModel.getFruits()
+            }
+        }
+    }
+    
 }
 
 
